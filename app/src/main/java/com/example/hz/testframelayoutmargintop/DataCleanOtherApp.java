53732ddad29b2d357e739c3669d179e1821ec745
wliuxingxiangyu <wliuxingxiangyu@163.com>
2017-08-06 22:34:45 +0800

    删app数据ok,,DataCleanManager.java

--- app/src/main/java/com/example/hz/testframelayoutmargintop/DataCleanOtherApp.java
+++ app/src/main/java/com/example/hz/testframelayoutmargintop/DataCleanOtherApp.java
@@ -3,14 +3,60 @@ package com.example.hz.testframelayoutmargintop;
 /**
  * Created by lenovo on 17/8/6.
  */
+
 import java.io.File;
+
+import android.app.ActivityManager;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Environment;
 import android.util.Log;
 
 public class DataCleanOtherApp {
+//    private ClearUserDataObserver mClearDataObserver;
+//
+//    class ClearUserDataObserver extends IPackageDataObserver.Stub {
+//        public void onRemoveCompleted(final String packageName, final boolean succeeded) {
+//            /*final Message msg = mHandler.obtainMessage(CLEAR_USER_DATA);
+//            msg.arg1 = succeeded?OP_SUCCESSFUL:OP_FAILED;
+//            mHandler.sendMessage(msg);*/
+//        }
+//    }
+//
+//    private Context getOtherAppContext(Context context, String packageName) {
+//        Context c = null;
+//        try {
+//            c = context.createPackageContext(packageName, Context.CONTEXT_INCLUDE_CODE | Context.CONTEXT_IGNORE_SECURITY);
+//        } catch (PackageManager.NameNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        return c;
+//    }
+//
+//    /*
+//     * Private method to initiate clearing user data when the user clicks the clear data
+//     * button for a system package
+//     */
+//    private void initiateClearUserData(Context context,String packageName) {
+//        // Invoke uninstall or clear user data based on sysPackage
+//        Log.i("type1", "Clearing user data for package : " + packageName);
+//        if (mClearDataObserver == null) {
+//            mClearDataObserver = new ClearUserDataObserver();
+//        }
+//        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+//        boolean res = am.clearApplicationUserData(packageName, mClearDataObserver);
+//        if (!res) {
+//            // Clearing data failed for some obscure reason. Just log error for now
+//            Log.i("type1", "Couldnt clear application user data for package:" + packageName);
+//
+//        } else {
+//            // success
+//            Log.i("type1", "success");
+//        }
+//    }
 
-    /**
+
+    /**---------------------------------------------------------------------------
      * 清除本应用所有数据库(/data/data/com.xxx.xxx/databases)
      *
      * @param context
@@ -36,8 +82,8 @@ public class DataCleanOtherApp {
      */
     private static void deleteFilesByDirectory(File directory) {
         if (directory != null && directory.exists() && directory.isDirectory()) {
-            if(directory.listFiles() != null && directory.listFiles().length != 0){
-                Log.d("hz-","directory.listFiles().length="+directory.listFiles().length);
+            if (directory.listFiles() != null && directory.listFiles().length != 0) {
+                Log.d("hz-", "directory.listFiles().length=" + directory.listFiles().length);
                 for (File item : directory.listFiles()) {
                     item.delete();
                 }
